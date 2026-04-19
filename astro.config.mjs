7313f1f4a110b9736858913e1f107821b1606b8a
MichaelXNS-0202 <wmevangelize7@gmail.com>
2026-04-18 21:44:22 -0500

    Add srcDir to Astro configuration
    Added srcDir property to Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-      output: 'static',
-      site: 'https://immanuel-laos-church.wmevangelize7.workers.dev',
+        output: 'static',
+        site: 'https://immanuel-laos-church.wmevangelize7.workers.dev',
+        srcDir: '.',
 });
